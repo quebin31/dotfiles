022293dc010052f9ccb930c7b2c795aaa09756e4
Kevin <quebin31@gmail.com>
2022-10-26 20:42:25 -0500

    chore: Remove Freetype version

--- profile/freetype2.sh
+++ profile/freetype2.sh
@@ -1,1 +1,1 @@
-export FREETYPE_PROPERTIES="truetype:interpreter-version=38 cff:no-stem-darkening=0 type1:no-stem-darkening=0 t1cid:no-stem-darkening=0"
+export FREETYPE_PROPERTIES="cff:no-stem-darkening=0 type1:no-stem-darkening=0 t1cid:no-stem-darkening=0"
