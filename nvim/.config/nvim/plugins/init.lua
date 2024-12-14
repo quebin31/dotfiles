1c1a6f47fbfd7776458b3ab6473566e87c6b3c2e
Kevin <quebin31@gmail.com>
2024-12-14 14:46:35 -0500

    chore: General clean up

--- nvim/.config/nvim/plugins/init.lua
+++ nvim/.config/nvim/plugins/init.lua
@@ -7,7 +7,6 @@ require('toggleterm').setup {
     open_mapping = [[<F1>]]
 }
 
-vim.g.mapleader = ','
 vim.keymap.set('n', '<leader>n', ':NvimTreeToggle<cr>', { silent = true, noremap = true})
 vim.keymap.set('n', '<leader>f', ':NvimTreeFindFileToggle<cr>', { silent = true, noremap = true})
 vim.keymap.set('n', '<leader>m', ':NvimTreeFocus<cr>', { silent = true, noremap = true})
