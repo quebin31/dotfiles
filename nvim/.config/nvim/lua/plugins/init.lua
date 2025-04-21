f93ee10efd4ff3ed254cda1e8a92625738e82f98
Kevin <quebin31@gmail.com>
2025-04-20 20:23:40 -0500

    chore: add telescope

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -122,8 +122,24 @@ return {
         },
         keys = {
             { '<leader>n', ':NvimTreeToggle<cr>' },
-            { '<leader>f', ':NvimTreeFindFile<cr>' },
+            { '<leader>g', ':NvimTreeFindFile<cr>' },
             { '<leader>m', ':NvimTreeFocus<cr>' }
+        }
+    },
+
+    -- Telescope
+    {
+        'nvim-telescope/telescope.nvim',
+        cond = is_nvim,
+        branch = '0.1.x',
+        dependencies = {
+            'nvim-lua/plenary.nvim'
+        },
+        keys = {
+            { '<leader>ff', ':Telescope find_files<cr>' },
+            { '<leader>fg', ':Telescope live_grep<cr>' },
+            { '<leader>fb', ':Telescope buffers<cr>' },
+            { '<leader>fh', ':Telescope help_tags<cr>' }
         }
     },
 }
